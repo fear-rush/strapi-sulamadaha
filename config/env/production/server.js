af76ee1c4f25adf583f51e9bf0e42c7e5065a862
Muhammad Firas <muhfiras1@gmail.com>
2022-08-08 22:16:45 +0700

    edit config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,11 +2,26 @@
 //  url: env('https://api-sulamadaha.herokuapp.com/')
 // });
 
+// module.exports = ({ env }) => ({
+//   url: env('https://api-sulamadaha.herokuapp.com/'),
+//   proxy: true,
+//   app: {
+//     keys: env.array("APP_KEYS"),
+//   },
+// });
+ 
 module.exports = ({ env }) => ({
-  url: env('https://api-sulamadaha.herokuapp.com/'),
-  proxy: true,
-  app: {
-    keys: env.array("APP_KEYS"),
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'takome'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'root'),
+    },
+    debug: false,
   },
 });
- 
+
+
