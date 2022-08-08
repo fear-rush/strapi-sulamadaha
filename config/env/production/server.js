cf0b67f34f5213f1733d83bb1f1ef49245a6d0d6
Muhammad Firas <muhfiras1@gmail.com>
2022-08-08 14:47:16 +0700

    add env config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,11 @@
+// module.exports = ({ env }) => ({
+//  url: env('https://api-sulamadaha.herokuapp.com/')
+// });
+
 module.exports = ({ env }) => ({
- url: env('https://api-sulamadaha.herokuapp.com/')
+  url: env('https://api-sulamadaha.herokuapp.com/'),
+  proxy: true,
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
 });
